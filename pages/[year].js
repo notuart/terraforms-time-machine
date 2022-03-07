54d72d79fb1815fea9e03cc04707f86f18d3d962
no+u <benoit.villiere@icloud.com>
2022-03-07 22:50:58 +0000

    MVP ready

--- pages/[year].js
+++ pages/[year].js
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import Link from 'next/link'
+import { useState } from 'react'
 
 export const getStaticProps = async () => {
   let years = []
@@ -35,9 +36,16 @@ export default function token({ years }) {
   const prev = current - 100 < 0 ? 5000 : current - 100
   const next = current + 100 > 5000 ? 0 : current + 100
 
+  const Terraform = ({ id }) => (
+    <iframe
+      className="flex w-1/4 aspect-square mt-24" 
+      src={`data/${current}/${id}.html`}
+    ></iframe>
+  )
+
   return (
-    <>
-      <div className="flex items-center justify-center py-3 border-b py-6">
+    <div className="flex w-full flex-col flex-wrap py-12">
+      <div className="fixed flex items-center justify-center py-3 border-b py-6 bg-neutral-900" style={{ top: 0, width: "100%" }}>
         <Link
           href={{
             pathname: '/[year]',
@@ -46,8 +54,8 @@ export default function token({ years }) {
             },
           }}
         >
-          <button className="rounded-xl bg-black border border-neutral-800 py-1 px-3">
-            Previous
+          <button className="rounded-xl bg-neutral-800 leading-loose border border-neutral-800 px-3 mx-6">
+            &larr; Previous
           </button>
         </Link>
 
@@ -61,24 +69,24 @@ export default function token({ years }) {
             },
           }}
         >
-          <button className="rounded-xl bg-black border border-neutral-800 py-1 px-3">
-            Next
+          <button className="rounded-xl bg-neutral-800 leading-loose border border-neutral-800 px-3 mx-6">
+            Next &rarr;
           </button>
         </Link>
 
-        <div className="self-end">
+        {/* <div className="self-end">
           Jump to:
           <select className="px-2 py-1 bg-black rounded">
             {years.map((y) => (
-              <option key={`select-year-${y}`} value={y}>{y}</option>
+              <option key={`select-year-${y}`} value={y} onChange={e => current}>{y}</option>
             ))}
           </select>
-        </div>
+        </div> */}
       </div>
       
-      <div>
-        <iframe src={`https://raw.githubusercontent.com/notuart/terraforms/main/terraforms-snapshots/001-decay/0/1.html`}></iframe>
+      <div class="aspect-video flex items-center flex-wrap flex-row">
+        {[...Array(12)].map((x, i) => <Terraform key={i} id={i + 1} />)}
       </div>
-    </>
+    </div>
   )
 }
